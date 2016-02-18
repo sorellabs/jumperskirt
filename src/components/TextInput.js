5a80ed8c7cb3d5e428e096f37b75c7fd9e68c4f4
Quildreen Motta <quildreen.ribeiro@plansis.com.br>
2016-02-18 00:57:26 -0200

    ...

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -10,66 +10,35 @@
 //
 //----------------------------------------------------------------------
 
-// # module: src/components/TextInput
-//
-// Text inputs
-module.exports = function(React) {
+module.exports = (React) => {
+  var classNames = require('classnames');
   var { Success, Failure } = require('data.validation');
   var Maybe = require('data.maybe');
-  var classSet = require('classnames');
+  var Field = require('./Field');
 
-  var T = React.PropTypes;
-  var Input = React.createFactory("input");
 
-  return React.createClass({
-    propTypes: {
-      classNames: T.arrayOf(T.string),
-      type: T.string,
-      autoComplete: T.string,
-      label: T.string,
-      placeholder: T.string,
-      description: T.string,
-      initialValue: T.string,
-      readOnly: T.bool,
-      validation: T.func,
-      onChange: T.func
-    },
-
-    getDefaultProps() {
-      return {
-        classNames: [],
-        placeholder: '',
-        description: '',
-        initialValue: '',
-        label: '',
-        autocomplete: 'off',
-        readOnly: false,
-        onChange(){ },
-        validation: value => Success(value)
+  class TextInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value     : props.initialValue,
+        error     : Maybe.Nothing(),
+        isFocused : false
       };
-    },
+    }
 
-    getInitialState() {
-      return {
-        value: this.props.initialValue,
-        error: Maybe.Nothing(),
-        isFocused: false
-      };
-    },
-
+    // -- Rendering ----------------------------------------------------
     render() {
-      var classes = classSet({
-        'jsk-field': true,
-        'jsk-text-field': true,
-        'jsk-focused': this.state.isFocused,
-        'jsk-non-editable': this.props.readOnly,
-        'jsk-has-text': this.state.value !== '',
-        'jsk-invalid': this.state.error.isJust
+      var classes = classNames({
+        'jsk-focused'      : this.state.isFocused,
+        'jsk-non-editable' : this.props.readOnly,
+        'jsk-has-text'     : this.state.value !== '',
+        'jsk-invalid'      : this.state.error.isJust
       }, this.props.classNames);
 
       return (
-        <div className={ classes }>
-          <label className="jsk-field-label" onClick={ this._onLabelClicked }>
+        <Field kind="jsk-text-field">
+          <label className="jsk-field-label" onClick={ ::this._onLabelClicked }>
             { this.props.label }
           </label>
           {
@@ -85,59 +54,104 @@ module.exports = function(React) {
               autocomplete: this.props.autoComplete
             })
           }
-        { this.state.error.map(this._renderError).getOrElse(null) }
-        <div className="jsk-field-description">{ this.props.description }</div>
-        </div>
+          {
+            this.state.error.map(::this._renderError).getOrElse(null)
+          }
+          <div className="jsk-field-description">
+            { this.props.description }
+          </div>
+        </Field>
       );
-    },
+    }
 
     _renderError(error) {
       return (
         <div className="jsk-field-validation-failure">
           { error }
         </div>
-      );
-    },
+      )
+    }
 
-    _onLabelClicked(e) {
+
+    // -- Events -------------------------------------------------------
+    _onLabelClicked(event) {
       this.refs.input.getDOMNode().focus();
-    },
+    }
 
-    _onInputChanged(e) {
+    _onInputChanged(event) {
       if (!this.props.readOnly) {
-        this.setValue(e.target.value);
+        this.setValue(event.target.value);
       }
-    },
+    }
 
     _onInvalidValue(error) {
-      this.setState({ error: Maybe.Just(error) });
-    },
+      this.setState({
+        error: Maybe.Just(error)
+      });
+    }
 
-    _onFocused(e) {
-      this.setState({ isFocused: true });
-    },
+    _onFocused(event) {
+      this.setState({
+        isFocused: true
+      });
+    }
 
-    _onBlurred(e) {
-      this.setState({ isFocused: false });
-    },
+    _onBlurred(event) {
+      this.setState({
+        isFocused: false
+      });
+    }
 
+
+    // -- API ----------------------------------------------------------
     setValue(newValue) {
       var oldValue = this.state.value;
       this.props.validation(newValue).cata({
-        Failure: this._onInvalidValue,
+        Failure: error =>
+          this._onInvalidValue(error),
+
         Success: data => {
           this.setState({ value: data, error: Maybe.Nothing() });
           this.props.onChange(newValue, oldValue);
         }
       });
-    },
+    }
 
     getValue() {
       return this.state.value;
-    },
+    }
 
     resetValue() {
       this.setValue(this.props.initialValue);
     }
-  });
+  }
+
+  TextInput.propTypes = {
+    classNames   : T.arrayOf(T.string),
+    type         : T.string,
+    autoComplete : T.string,
+    label        : T.string,
+    placeholder  : T.string,
+    description  : T.string,
+    initialValue : T.string,
+    readOnly     : T.bool,
+    validation   : T.func,
+    onChange     : T.func
+  };
+
+  TextInput.defaultProps = {
+    classNames   : [],
+    type         : 'text',
+    placeholder  : '',
+    description  : '',
+    initialValue : '',
+    label        : '',
+    autoComplete : 'off',
+    readOnly     : false,
+    onChange     : (newValue, oldValue) => null,
+    validation   : (value) => Success(value)
+  };
+
+  return TextInput;
 };
+
